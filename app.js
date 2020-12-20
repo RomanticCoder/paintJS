a8a3d39d863522aad500a67191ee6c520035ff18
RomanticCoder <67548512+RomanticCoder@users.noreply.github.com>
2020-12-20 00:10:43 -0500

    (added) a function to draw a rectangle

--- app.js
+++ app.js
@@ -1,7 +1,11 @@
 const canvas = document.getElementById("jsCanvas");
 const ctx = canvas.getContext("2d");
-
+const saveBtn = document.getElementById("jsSave");
+const jsMode = document.getElementById("jsMode");
+const jsRect = document.getElementById("jsRect");
 const range = document.getElementById("jsRange");
+const currentColor = document.getElementById("controls__current_color");
+const jscolors = document.getElementsByClassName("controls__color");
 
 canvas.width = canvas.offsetWidth;
 canvas.height = canvas.offsetHeight;
@@ -11,34 +15,57 @@ ctx.fillStyle = "white";
 ctx.rect(0, 0, canvas.width, canvas.height);
 ctx.fill();    
 ctx.lineWidth = range.value;
-
+ctx.fillStyle = "black";
 let painting = false;
 let filling = false;
 let rectangle = false;
+
+let beginX = 0;
+let beginY = 0;
 function stopPainting(event){
     painting = false;
+    console.log("stopped");
+    if(rectangle === true){
+        const x = event.offsetX;
+        const y = event.offsetY;
+        ctx.strokeRect(beginX, beginY, x-beginX, y-beginY);
+        console.log("stopped, x:"+beginX +" y:"+ beginY+" x:"+x+" y:"+y);
+
+    }
 }
 
-function startPainting(){
+function startPainting(event){
     painting = true;
+    if(rectangle === true){
+        beginX = event.offsetX;
+        beginY = event.offsetY;
+    }
+    console.log("started"+ beginX + beginY);
+
 }
 
 function onMouseMove(event){
     const x = event.offsetX;
     const y = event.offsetY;
-    if(!painting){
-        ctx.beginPath();
-        ctx.moveTo(x,y);
-    }else{
-        ctx.lineTo(x,y);
-        ctx.stroke();
-        // ctx.closePath(); -> fix the starting point
+    if(!rectangle){
+        if(!painting){
+            console.log("p:"+painting);
+            ctx.beginPath();
+            ctx.moveTo(x,y);
+        }
+        else{
+            console.log("p:"+painting);
+            ctx.lineTo(x,y);
+            ctx.stroke();
+            // ctx.closePath(); -> fix the starting point
+        }
     }
 }
 
 function handleColorClick(event){
     ctx.strokeStyle = event.target.style.backgroundColor;
     ctx.fillStyle = event.target.style.backgroundColor;
+    currentColor.style.backgroundColor = event.target.style.backgroundColor;
 }
 
 function handleRangeChange(event){
@@ -58,10 +85,21 @@ function handleModeClick(event){
     if(filling === true){
         filling = false;
         jsMode.innerHTML = "fill";
-    }else{
+    }else{ 
         filling = true;
         jsMode.innerHTML = "paint";
     }    
+}
+
+function handleRectClick(event){
+    if(rectangle === true){
+        rectangle = false;
+        jsRect.style.backgroundColor = "white";
+
+    }else{
+        rectangle = true;
+        jsRect.style.backgroundColor = "gray";
+    }
 }
 
 function handleCM(event){
@@ -90,7 +128,6 @@ if (canvas) {
 // const colors = document.getElementById("jsColors");
 // colors.addEventListener("click",handleColorClick);
 
-const jscolors = document.getElementsByClassName("controls__color");
 Array.from(jscolors).forEach(color=>{
     color.addEventListener("click",handleColorClick);
 })
@@ -99,11 +136,12 @@ if(range){
     range.addEventListener("change",handleRangeChange)
 }
 
-const jsMode = document.getElementById("jsMode");
 jsMode.addEventListener("click",handleModeClick);
-
-const saveBtn = document.getElementById("jsSave");
 
 if(saveBtn){
     saveBtn.addEventListener("click",handleSaveClick);
+}
+
+if(jsRect){
+    jsRect.addEventListener("click", handleRectClick);
 }
